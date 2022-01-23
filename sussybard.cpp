a01ebe462ad80f0be2390d0d09ee1c13ff0bb90c
Hans-Kristian Arntzen <maister@archlinux.us>
2022-01-23 12:28:40 +0100

    Seems to work?

--- sussybard.cpp
+++ sussybard.cpp
@@ -1,5 +1,37 @@
 #include <alsa/asoundlib.h>
 #include <stdio.h>
+#include <xcb/xcb.h>
+#include <xcb/xtest.h>
+#include <xcb/xcb_keysyms.h>
+#include <stdlib.h>
+#include <vector>
+
+static xcb_keycode_t get_keycode(xcb_key_symbols_t *syms, xcb_keysym_t c)
+{
+	xcb_keycode_t *codes = xcb_key_symbols_get_keycode(syms, c);
+
+	if (!codes)
+	{
+		fprintf(stderr, "Failed to query keycode for %u.\n", c);
+		exit(EXIT_FAILURE);
+	}
+
+	if (codes[0] == XCB_NO_SYMBOL)
+	{
+		fprintf(stderr, "No symbol found for %u.\n", c);
+		exit(EXIT_FAILURE);
+	}
+
+	if (codes[1] != XCB_NO_SYMBOL)
+	{
+		fprintf(stderr, "Unexpected multi-key symbol for %u.\n", c);
+		exit(EXIT_FAILURE);
+	}
+
+	xcb_keycode_t code = codes[0];
+	free(codes);
+	return code;
+}
 
 static void list_midi_ports(snd_seq_t *seq)
 {
@@ -85,6 +117,95 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
+	xcb_connection_t *conn = xcb_connect(nullptr, nullptr);
+	if (!conn)
+	{
+		fprintf(stderr, "Unable to connect to X server.\n");
+		return EXIT_FAILURE;
+	}
+
+	xcb_generic_error_t *err = nullptr;
+	xcb_test_get_version_cookie_t cookie = xcb_test_get_version(conn, 2, 1);
+	xcb_test_get_version_reply_t *reply = xcb_test_get_version_reply(conn, cookie, &err);
+
+	if (reply)
+	{
+		printf("Got XTEST version %u.%u.\n", reply->major_version, reply->minor_version);
+		free(reply);
+	}
+
+	if (err)
+	{
+		printf("Got error: %d.\n", err->error_code);
+		free(err);
+	}
+
+	xcb_window_t win = xcb_setup_roots_iterator(xcb_get_setup(conn)).data->root;
+
+	// 3 octave range for Bard.
+	constexpr int base_key = 36; // C somewhere on my keyboard.
+	constexpr int num_octaves = 3;
+	constexpr int num_keys = num_octaves * 12;
+
+	xcb_key_symbols_t *syms = xcb_key_symbols_alloc(conn);
+	struct Event
+	{
+		xcb_keycode_t code;
+		uint8_t type;
+	};
+	std::vector<std::vector<Event>> code_tables(num_keys);
+	for (int octave = 0; octave < num_octaves; octave++)
+	{
+		int base = octave * 12;
+		constexpr xcb_keysym_t XK_Shift_L = 0xffe1;
+		constexpr xcb_keysym_t XK_Control_L = 0xffe3;
+
+		if (octave == 0)
+		{
+			xcb_keycode_t down = get_keycode(syms, XK_Control_L);
+			for (int j = 0; j < 12; j++)
+				code_tables[base + j].push_back({ down, XCB_KEY_PRESS });
+		}
+		else if (octave == 2)
+		{
+			xcb_keycode_t up = get_keycode(syms, XK_Shift_L);
+			for (int j = 0; j < 12; j++)
+				code_tables[base + j].push_back({ up, XCB_KEY_PRESS });
+		}
+
+		const auto add_key_press_release = [&](int i, xcb_keysym_t c) {
+			xcb_keycode_t code = get_keycode(syms, c);
+			code_tables[base + i].push_back({ code, XCB_KEY_PRESS });
+			code_tables[base + i].push_back({ code, XCB_KEY_RELEASE });
+		};
+
+		add_key_press_release(0, 'q'); // C
+		add_key_press_release(1, '2'); // C#
+		add_key_press_release(2, 'w'); // D
+		add_key_press_release(3, '3'); // D#
+		add_key_press_release(4, 'e'); // E
+		add_key_press_release(5, 'r'); // F
+		add_key_press_release(6, '5'); // F#
+		add_key_press_release(7, 't'); // G
+		add_key_press_release(8, '6'); // G#
+		add_key_press_release(9, 'y'); // A
+		add_key_press_release(10, '7'); // A#
+		add_key_press_release(11, 'u'); // B
+
+		if (octave == 0)
+		{
+			xcb_keycode_t down = get_keycode(syms, XK_Control_L);
+			for (int j = 0; j < 12; j++)
+				code_tables[base + j].push_back({ down, XCB_KEY_RELEASE });
+		}
+		else if (octave == 2)
+		{
+			xcb_keycode_t up = get_keycode(syms, XK_Shift_L);
+			for (int j = 0; j < 12; j++)
+				code_tables[base + j].push_back({ up, XCB_KEY_RELEASE });
+		}
+	}
+
 	snd_seq_event_t *ev = nullptr;
 	for (;;)
 	{
@@ -97,15 +218,28 @@ int main(int argc, char **argv)
 
 		if (ev->type == SND_SEQ_EVENT_NOTEON && ev->data.note.velocity > 0)
 		{
-			printf("Note on! %d (vel = %d)\n", ev->data.note.note, ev->data.note.velocity);
+			//printf("Note on! %d (vel = %d)\n", ev->data.note.note, ev->data.note.velocity);
+			int node_offset = int(ev->data.note.note) - base_key;
+			if (node_offset >= 0 && node_offset < num_keys)
+			{
+				auto &chain = code_tables[node_offset];
+				for (auto &input : chain)
+					xcb_test_fake_input(conn, input.type, input.code, 0, win, 0, 0, 0);
+			}
+			xcb_flush(conn);
 		}
+#if 0
+		// We cannot sustain notes in FF, so don't bother for now.
 		else if (ev->type == SND_SEQ_EVENT_NOTEOFF || (ev->type == SND_SEQ_EVENT_NOTEON && ev->data.note.velocity == 0))
 		{
 			printf("Note off! %d\n", ev->data.note.note);
 		}
+#endif
 
 		snd_seq_free_event(ev);
 	}
 
 	snd_seq_close(seq);
+	xcb_key_symbols_free(syms);
+	xcb_disconnect(conn);
 }
